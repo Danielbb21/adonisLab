8fc09e7df5e0bd4f0e5447392a0c0165be443d75
Daniel <danielbb2000@gmail.com>
2021-08-20 13:22:17 -0300

    Completed class 16

--- app/Validators/User.js
+++ app/Validators/User.js
@@ -8,7 +8,7 @@ class User {
   get rules () {
     return {
       username: 'required|unique:users',
-      email: 'required|email|unique',
+      email: 'required|email|unique:users',
       password: 'required|confirmed'
     }
   }
